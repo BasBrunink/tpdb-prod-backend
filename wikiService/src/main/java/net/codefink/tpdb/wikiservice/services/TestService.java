5ba3bd84e0841a774f46ea13b09e94f1f64787ad
BasBrunink <115614124+BasBrunink@users.noreply.github.com>
2024-06-05 11:05:05 +0200

    [VERIFY] project tests all stuff

---
+++ wikiService/src/main/java/net/codefink/tpdb/wikiservice/services/TestService.java
@@ -0,0 +1,15 @@
+package net.codefink.tpdb.wikiservice.services;
+
+public class TestService {
+
+  public Integer add(Integer a, Integer b) {
+    return a + b;
+  }
+
+  public Integer subtract(Integer a, Integer b) {
+    return a - b;
+  }
+  public Integer multiply(Integer a, Integer b) {
+    return a * b;
+  }
+}
--- wikiService/src/test/java/net/codefink/tpdb/wikiservice/WikiServiceTests.java
+++ wikiService/src/test/java/net/codefink/tpdb/wikiservice/WikiServiceTests.java
@@ -1,9 +1,25 @@
 package net.codefink.tpdb.wikiservice;
 
+import net.codefink.tpdb.wikiservice.services.TestService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WikiServiceTests {
 
+  @InjectMocks
+  private TestService wikiService;
+
+  @BeforeEach
+  public void setup() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void testAdd() {
+    assertEquals(3, wikiService.add(1, 2));
+  }
 }
